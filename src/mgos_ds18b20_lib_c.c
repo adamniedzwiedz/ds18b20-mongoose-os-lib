10fbe2b760796e6661c9b617d2f5968803d51c88
Adam Niedzwiedz <niedzwiedz.adam@gmail.com>
2018-08-07 17:40:45 +0200

    fix

--- src/mgos_ds18b20_lib_c.c
+++
@@ -1,5 +0,0 @@
-#include <stdbool.h>
-
-bool mgos_ds18b20_lib_init(void) {
-    return true;
-}
---
+++ src/mgos_ds18b20_mongoose_os_lib_c.c
@@ -0,0 +1,5 @@
+#include <stdbool.h>
+
+bool mgos_ds18b20_mongoose_os_lib_init(void) {
+    return true;
+}
